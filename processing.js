7117298fbe7f2c23e6827d2698486788e9d7b599
Archie <archanasahota@gmail.com>
2011-11-30 23:41:03 -0500

    update

--- processing.js
+++ processing.js
@@ -3025,10 +3025,10 @@
        * @member PShape
        * The drawPrimitive() function draws SVG document shape elements. These can be point, line, triangle, quad, rect, ellipse, arc, box, or sphere.
        */
-      drawPrimitive: function() {     
+      drawPrimitive: function() {
         if (this.kind === PConstants.POINT) {
           p.point(this.params[0], this.params[1]);
-        } else if (this.kind === PConstants.LINE) {	
+        } else if (this.kind === PConstants.LINE) {
           if (this.params.length === 4) {  // 2D
             p.line(this.params[0], this.params[1],
                    this.params[2], this.params[3]);
@@ -4544,12 +4544,10 @@
               p.translate(x, y);
             }
           }
-
           shape.draw();
-          //set them back to what they were before
+          //set the values back to what they were before
           curRectMode = rcMode;
           curEllipseMode = elMode;
-
           if ((arguments.length === 1 && curShapeMode === PConstants.CENTER ) || arguments.length > 1) {
             p.popMatrix();
           }
